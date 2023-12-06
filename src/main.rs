993c78150cbc57180f3834c7d11bbe7d0fd77acf
Rainweic <861514706@qq.com>
2023-12-06 23:52:55 +0800

    [update] add println!

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,25 @@
 
 mod vga_buffer;
 
+
+use core::fmt::Write;
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
 
 /// 这个函数将在 panic 时被调用
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle] // 不重整函数名
 pub extern "C" fn _start() -> ! {
 
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            // 共2个byte用于显示
-            // 前一个byte(8个bit)显示ASCII码
-            // 后一个byte(8个bit)显示颜色
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
 
     loop {}
 
 }
+
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,7 @@
+use core::fmt;
+use lazy_static::lazy_static;
+use spin::Mutex;
+use volatile::Volatile;
 
 #[allow(dead_code)]                                 // 对未使用的变量, 不显示警告
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -42,12 +46,13 @@ struct ScreenChar {
 }
 
 
+// 从上到下 是第0..79行
 const BUFFER_WIDTH: usize = 80;
 const BUFFER_HEIGHT: usize = 25;
 
 /// Buffer shape: (25, 80)
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT]
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT]
 }
 
 
@@ -58,6 +63,15 @@ pub struct Writer {
 }
 
 impl Writer {
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
     pub fn write_byte(&mut self, byte: u8) {
         match byte {
             b'\n' => self.new_line(),
@@ -67,14 +81,14 @@ impl Writer {
                     self.new_line();
                 }
 
-                let row = BUFFER_HEIGHT - 1;
+                let row = BUFFER_HEIGHT - 1;       // 始终是往最底下那一行写内容
                 let col = self.column_position;
 
                 let color_code = self.color_code;
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
                     color_code
-                };
+                });
 
                 self.column_position += 1;
             }
@@ -82,17 +96,66 @@ impl Writer {
     }
 
     fn new_line(&mut self) {
-        todo!()
+
+        // Step 1
+        // 从第1行开始到第79行 每一行都读取一个字符 复制给上一行
+        // 例如第i行第j列的char读取出来 赋值给第i - 1行第j列
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+
+        // Step 2
+        // 清空最后一行
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
     }
 }
 
-impl Writer {
-    pub fn write_string(&mut self, s: &str) {
-        for byte in s.bytes() {
-            match byte {
-                0x20..=0x7e | b'\n' => self.write_byte(byte),
-                _ => self.write_byte(0xfe),
-            }
-        }
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
     }
+}
+
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
+
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
 }
