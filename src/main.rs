9607ccb6abba87b6ee947582700806a8c7cabdef
Rainweic <861514706@qq.com>
2023-12-11 14:11:26 +0800

    [update] exit qemu

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
 #![no_std] // 不链接 Rust 标准库
 #![no_main] // 禁用所有 Rust 层级的入口点
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 
 mod vga_buffer;
 
 
 use core::fmt::Write;
 use core::panic::PanicInfo;
+use x86_64::instructions::port::Port;
+
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 
 static HELLO: &[u8] = b"Hello World!";
 
@@ -16,10 +36,29 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test()
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
 #[no_mangle] // 不重整函数名
 pub extern "C" fn _start() -> ! {
 
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 
